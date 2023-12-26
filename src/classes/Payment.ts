48da564a08320e1a7c68e801460ab20fc8e933ba
Swanand Mathekar <swanandmathekar@gmail.com>
2023-12-26 16:47:51 +0530

    Add Payment class

---
+++ src/classes/Payment.ts
@@ -0,0 +1,19 @@
+import HasFormatter from "../interfaces/HasFormatter";
+
+class Payment implements HasFormatter {
+    readonly recipient: string;
+    private details: string;
+    public amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.recipient = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.recipient} is owed £${this.amount} for ${this.details}`;
+    }
+}
+
+export default Payment;
